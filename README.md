5c65b6c689a0f8bd7abd34068ee2e9546323fcf9
Filipp Sidorov <filippsidorov@yandex.ru>
2024-04-26 16:43:39 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hammingway Mode
+
+## Description:
+Hammingway Mode is an Emacs package that allows you to toggle the functionality of the backspace key with the keystroke Control-C h m. This mode is designed to enhance your editing experience by easily enabling or disabling the backspace key as needed.
+
+## Usage:
+To toggle Hammingway Mode on and off, simply press Control-C h m. When the mode is on, the backspace key functionality will be disabled, and when the mode is off, the backspace key will function as usual.
+
+## Installation:
+1. Download the `hammingway.el` file from this repository.
+2. Add `(load "/path/to/hammingway.el")` to your Emacs configuration file (`.emacs` or `init.el`).
+3. Restart Emacs or evaluate the new configuration.
+4. Toggle Hammingway Mode on and off using Control-C h m.
---
+++ hammingway.el
@@ -0,0 +1,27 @@
+(defvar hammingway-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "DEL") 'delete-backward-char)
+    map)
+  "Keymap for Hammingway Mode.")
+
+(define-minor-mode hammingway-mode
+  "Toggle Hammingway mode."
+  :init-value nil
+  :lighter " Hammingway"
+  :keymap hammingway-mode-map
+  :group 'hammingway)
+
+(define-globalized-minor-mode global-hammingway-mode hammingway-mode hammingway-mode)
+
+(defun hammingway-toggle-backspace ()
+  (interactive)
+  (if (eq (key-binding (kbd "DEL")) 'delete-backward-char)
+      (progn
+        (define-key hammingway-mode-map (kbd "DEL") 'ignore)
+        (message "Hammingway Mode: off"))
+    (progn
+      (define-key hammingway-mode-map (kbd "DEL") 'delete-backward-char)
+      (message "Hammingway Mode: on"))))
+
+(define-key global-map (kbd "C-c h m") 'hammingway-toggle-backspace)
+(global-hammingway-mode 1)
